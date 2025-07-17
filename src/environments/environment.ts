50db639e6c01711aa196b56d70bb9285ff1b82ea
dariocrsng <dariocrsng@gmail.com>
2025-07-17 13:46:23 -0300

    cambiada base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  mockApiBaseUrl: 'https://mock-api.alambrito.com.ar',
+  production: true,
+  mockApiBaseUrl: 'https://alambrito.com.ar/api',
   defaultPageSize: 36,
   minimumSkeletonAnimationTime: 200
 };
